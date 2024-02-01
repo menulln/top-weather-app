06871c7aa349cc3b76c121a1dd0a1c3f1e25ab01
menulln <39300523+menulln@users.noreply.github.com>
2024-02-01 13:42:46 +0100

    Add formatWeather function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+function formatWeather(weatherObject) {
+    return {
+        condition: { ...weatherObject.current.condition },
+        isDay: weatherObject.current.is_day,
+        tempC: weatherObject.current.temp_c,
+        tempF: weatherObject.current.temp_f,
+        country: weatherObject.location.country,
+        city: weatherObject.location.name,
+        time: weatherObject.location.localtime,
+    };
+}
+
 async function getWeather(cityName) {
     const response = await fetch(
         `http://api.weatherapi.com/v1/current.json?key=71cbb440aad5415f836161300242901&q=${cityName}`
